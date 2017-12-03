e8ba7e1bb275624b90cd1ebabbb10a287baad386
Lisa Lee <snickers495@gmail.com>
2017-12-03 17:43:05 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,8 @@ class PostsController < ApplicationController
 
     if @post.update(post_params)
       redirect_to post_path(@post)
-    else 
-      render :edit 
+    else
+      render :edit
     end
   end
 
